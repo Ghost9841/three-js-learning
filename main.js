3e6f2c64607c4b1cc00e9eb2e3b24380c8fe9c11
Ghost9841 <asubedi778@gmail.com>
2025-05-03 22:21:53 +0545

    3d Cube Rotating

--- main.js
+++ main.js
@@ -10,22 +10,22 @@ camera.position.set(0,0,4);
 camera.lookAt(0,0,0);
 
 
-const lines = new THREE.Group();
-const material1 = new THREE.LineBasicMaterial({color: 0xffffff});
-const points = [];
-points.push(new THREE.Vector3(100, 100, 100));
-points.push(new THREE.Vector3(1,1,0));
-points.push(new THREE.Vector3(10,0,0));
-const geometry1 = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(geometry1,material1);
-lines.add(line);
+// const lines = new THREE.Group();
+// const material1 = new THREE.LineBasicMaterial({color: 0xffffff});
+// const points = [];
+// points.push(new THREE.Vector3(100, 100, 100));
+// points.push(new THREE.Vector3(1,1,0));
+// points.push(new THREE.Vector3(10,0,0));
+// const geometry1 = new THREE.BufferGeometry().setFromPoints(points);
+// const line = new THREE.Line(geometry1,material1);
+// lines.add(line);
 
-const lineInstance = lines.clone();
-scene.add(lineInstance);
+// const lineInstance = lines.clone();
+// scene.add(lineInstance);
 
-const lineInstance2 = lines.clone();
-lineInstance2.position.set(-10,0,0);
-scene.add(lineInstance2);
+// const lineInstance2 = lines.clone();
+// lineInstance2.position.set(-10,0,0);
+// scene.add(lineInstance2);
 
 
 // 3. Create and add a cube object
@@ -51,12 +51,18 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
+let colorIndex = 0;
+const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff'];
 
 //6. Animate the scene
 function animate () {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    colorIndex = (colorIndex + 0.1) % colors.length;
+    const color = colors[Math.floor(colorIndex)];
+    cube.material.color.set(color);
 
     renderer.render(scene,camera);
 
